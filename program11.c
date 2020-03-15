fb8bc2af5d39c4c54faeb44ab07074bdf3dc4ae8
Anil <40522274+anil-gc@users.noreply.github.com>
2020-03-15 14:14:18 +0530

    creating table part

---
+++ program11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int alloaction_A[3] = {0,3,2};
+    int allocation_B[3] = {0,2,1};
+    int allocation_C[3] = {1,0,1};
+
+    int maxNeed_A[3] = {8,4,3};
+    int maxNeed_B[3] = {6,2,0};
+    int maxNeed_C[3] = {3,3,3};
+
+    int available_A[3] = {0,3,2};
+    int available_B[3] = {0,2,1};
+    int available_C[3] = {1,0,1};
+
+
+}

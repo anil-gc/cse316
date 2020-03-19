b01940b8598a21632b5950f4680f38f4912cde7a
Anil <40522274+anil-gc@users.noreply.github.com>
2020-03-19 15:05:12 +0530

    working on step 2

--- program11.c
+++ program11.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    int work[3][3] = {{0,0,1},{3,2,0},{2,1,1}};//allocation
+    int allocation[3][3] = {{0,0,1},{3,2,0},{2,1,1}};//allocation
     int maxNeed[3][3] = {{8,4,3},{6,2,0},{3,3,3}};
-    int available[3]={3,2,2};
+    int work[3]={3,2,2}; //available
     int remaningNeed[3][3];
+    int safe;
     bool final[3] = {false,false,false};
-    printf(work[1][1]);
 
 // this is for remaning need
 
@@ -20,12 +20,35 @@ int main()
         }
     }
 
-for(int i=0;i<3;i++)
-{
-    if(final[i] == false)
+    for (int i = 0; i < 3; i++)
     {
-        maxNeed
+        if(final == false)
+        {   int checking = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                if(remaningNeed[i][j] <= work[j]) //checking steps 2
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+                checking += 1;
+                
+            }
+            if(checking == 3)
+            {
+                
+            }
+            
+        }
+        else
+        {
+            
+        }
+        
     }
-}
+
 
 }

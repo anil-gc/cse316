4017574c8606e49c8f3104a938dd891c887ecdb0
Anil <40522274+anil-gc@users.noreply.github.com>
2020-03-23 12:55:33 +0530

    working on algorithms

--- program11.c
+++ program11.c
@@ -5,9 +5,9 @@ int main()
 {
     int allocation[3][3] = {{0,0,1},{3,2,0},{2,1,1}};//allocation
     int maxNeed[3][3] = {{8,4,3},{6,2,0},{3,3,3}};
-    int work[3]={3,2,2}; //available
+    int available[3]={3,2,2}; //work
     int remaningNeed[3][3];
-    int safe;
+    int check =1;
     bool final[3] = {false,false,false};
 
 // this is for remaning need
@@ -19,34 +19,9 @@ int main()
             remaningNeed[i][j] = maxNeed[i][j] + work[i][j];
         }
     }
-
-    for (int i = 0; i < 3; i++)
-    {
-        if(final == false)
-        {   int checking = 0;
-            for (int j = 0; j < 3; j++)
-            {
-                if(remaningNeed[i][j] <= work[j]) //checking steps 2
-                {
-                    continue;
-                }
-                else
-                {
-                    break;
-                }
-                checking += 1;
-                
-            }
-            if(checking == 3)
-            {
-                
-            }
-            
-        }
-        else
-        {
-            
-        }
+// remaning need end
+    
+    while (check) {
         
     }
 
